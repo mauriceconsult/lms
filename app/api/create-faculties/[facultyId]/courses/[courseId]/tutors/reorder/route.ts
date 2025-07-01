6be058fbb7f2313b5159375b098f2a0c8764e2d5
Maurice Consultant <mainna62@outlook.com>
2025-07-01 15:16:59 +0300

    fixed amount route

--- app/api/create-faculties/[facultyId]/courses/[courseId]/tutors/reorder/route.ts
+++ app/api/create-faculties/[facultyId]/courses/[courseId]/tutors/reorder/route.ts
@@ -53,7 +53,7 @@ export async function PUT(
     }
     return new NextResponse("Success", { status: 200 });
   } catch (error) {
-    console.log("[REORDER]", error);
+    console.log("[TUTOR_REORDER]", error);
     return new NextResponse("Internal Error", { status: 500 });
   }
 }
