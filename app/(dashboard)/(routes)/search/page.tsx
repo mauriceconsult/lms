600a48fa22aac0c3f0d04b0553a4ce6059d7d1a2
Maurice Consultant <mainna62@outlook.com>
2025-07-23 17:49:16 +0300

    streamlined search

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -2,10 +2,8 @@ import { db } from "@/lib/db";
 import { Schools } from "./_components/schools";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
-import { RouteSearchInput } from "./_components/route-search-input";
 import { getFaculties } from "@/actions/get-faculties";
 import { FacultiesList } from "./_components/faculties-list";
-
 
 interface SearchPageProps {
   searchParams: Promise<{
@@ -13,9 +11,7 @@ interface SearchPageProps {
     schoolId: string;
   }>;
 }
-const RouteSearchPage = async ({
-  searchParams
-}: SearchPageProps) => {
+const RouteSearchPage = async ({ searchParams }: SearchPageProps) => {
   const { userId } = await auth();
   if (!userId) {
     return redirect("/");
@@ -27,16 +23,13 @@ const RouteSearchPage = async ({
   });
   const faculties = await getFaculties({
     userId,
-    ... await searchParams
-  })
+    ...(await searchParams),
+  });
   return (
     <>
-      <div className="px-6 pt-6 md:hidden md:mb-0 block">
-        <RouteSearchInput />
-      </div>
       <div className="p-6 space-y-4">
-        <Schools items={schools} />  
-        <FacultiesList items={faculties}/>
+        <Schools items={schools} />
+        <FacultiesList items={faculties} />
       </div>
     </>
   );
