71ac9f9cdfb0affdc27a04f35c259fcbaa39cc24
Maurice Consultant <mainna62@outlook.com>
2025-07-27 17:12:40 +0300

    adjusted sidebar

--- app/(faculty)/faculties/[facultyId]/_components/faculty-sidebar.tsx
+++ app/(faculty)/faculties/[facultyId]/_components/faculty-sidebar.tsx
@@ -1,27 +1,5 @@
-// "use client";
-
-// import { Logo } from "@/app/(dashboard)/_components/logo";
-// import { FacultySidebarRoutes } from "./faculty-sidebar-routes";
-
-// // interface FacultySidebarProps {
-// //   facultyId?: string; // Match FacultySidebarRoutes and FacultyNavbarRoutes
-// // }
-
-// export const FacultySidebar = () => {
-//   return (
-//     <div className="h-full border-r flex flex-col overflow-y-auto bg-white shadow-sm">
-//       <div className="p-6">
-//         <Logo />
-//       </div>
-//       <div>
-//         <FacultySidebarRoutes/>
-//       </div>
-//     </div>
-//   );
-// };
-
 "use client";
-
+import { Logo } from "@/app/(dashboard)/_components/logo";
 import { FacultySidebarRoutes } from "./faculty-sidebar-routes";
 
 interface FacultySidebarProps {
@@ -30,8 +8,14 @@ interface FacultySidebarProps {
 
 export const FacultySidebar = ({ facultyId }: FacultySidebarProps) => {
   return (
-    <div className="h-full border-r bg-white shadow-sm w-64 p-4">
-      <FacultySidebarRoutes facultyId={facultyId} />
+    <div className="h-full border-r flex flex-col overflow-y-auto bg-white shadow-sm">
+      <div className="p-6">
+        <Logo />
+      </div>
+
+      <div className="h-full border-r bg-white shadow-sm w-64 p-4">
+        <FacultySidebarRoutes facultyId={facultyId} />
+      </div>
     </div>
   );
 };
