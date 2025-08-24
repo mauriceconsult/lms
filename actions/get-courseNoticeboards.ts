1ac1c5286045b0a7b8345d19c4f1073910135d58
Maurice Consultant <mainna62@outlook.com>
2025-08-25 01:13:22 +0300

    fixed ts in course notice

--- actions/get-courseNoticeboards.ts
+++ actions/get-courseNoticeboards.ts
@@ -1,4 +1,3 @@
-// actions/get-courseNoticeboards.ts
 import { db } from "@/lib/db";
 
 interface GetCourseNoticeboardsParams {
@@ -6,18 +5,25 @@ interface GetCourseNoticeboardsParams {
   courseCourseCourseNoticeboardId?: string;
   courseId?: string;
   adminId?: string;
+  userId?: string; // Add userId
 }
 
 export const getCourseNoticeboards = async (
   params: GetCourseNoticeboardsParams
 ) => {
   try {
-    const { title, courseCourseCourseNoticeboardId, courseId, adminId } =
-      params;
+    const {
+      title,
+      courseCourseCourseNoticeboardId,
+      courseId,
+      adminId,
+      userId,
+    } = params;
 
     const courseNoticeboards = await db.courseNoticeboard.findMany({
       where: {
         courseId,
+        userId, // Add userId filter
         course: { adminId },
         ...(title && { title: { contains: title, mode: "insensitive" } }),
         ...(courseCourseCourseNoticeboardId && {
